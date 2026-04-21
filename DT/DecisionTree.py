6fd9ea73f77fe9f174df4339466dcafde26ba658
Amir <amirtamer31@gmail.com>
2026-04-21 21:05:39 +0200

    Added classWeights for Dt

--- DT/DecisionTree.py
+++ DT/DecisionTree.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 class DecisionTree:
     class Node:
-        def __init__(self , feature = None , threshold = None , left = None , right = None ,decision= None):
+        def __init__(self , feature = None , threshold = None , left = None , right = None ,decision= None ):
             self.feature = feature
             self.threshold = threshold
             self.left = left
@@ -10,13 +10,15 @@ class DecisionTree:
             self.decision = decision
 
             
-    def __init__(self , maxDepth , minSamplesSplit , criterion , maxFeatures , minSampleLeafs):
+    def __init__(self , maxDepth , minSamplesSplit , criterion , maxFeatures , minSampleLeafs ,classWeights = None ):
         self.maxDepth = maxDepth
         self.minSamplesSplit = minSamplesSplit
         self.minSampleLeafs = minSampleLeafs
         self.criterion = criterion
         self.maxFeatures = maxFeatures
         self.allowedCriteria = ["entropy" , "gini"]
+        self.classWeights = classWeights
+
         if criterion not in self.allowedCriteria:
             raise ValueError("Criterion has to be either entropy or gini")
         self.root = None
@@ -55,16 +57,26 @@ class DecisionTree:
             rightEntropy = 0
             leftLabelCount = np.sum(leftLabelCounts)
             rightLabelCount = np.sum(rightLabelCounts)
-            for count in leftLabelCounts:
+            if self.classWeights!=None:
+                weightsArr = np.array([self.classWeights.get(i, 1)
+                               for i in range(leftLabelCounts.size)])
+            else:
+                weightsArr = np.ones(leftLabelCounts.size)
+            weightedCountsL = weightsArr * leftLabelCounts
+            weightedCountsR = weightsArr * rightLabelCounts
+            wLeftLabelCount = np.sum(weightedCountsL)
+            wRightLabelCount = np.sum(weightedCountsR)
+
+            for count in weightedCountsL:
                 if count == 0: continue
-                prob = count/leftLabelCount
+                prob = count/wLeftLabelCount
                 if self.criterion == 'entropy':
                     leftEntropy += -prob*np.log2(prob)
                 else:
                     leftEntropy += pow(prob , 2)
-            for count in rightLabelCounts:
+            for count in weightedCountsR:
                 if count == 0: continue
-                prob = count/rightLabelCount
+                prob = count/wRightLabelCount
                 if self.criterion == 'entropy':
                     rightEntropy += -prob*np.log2(prob)
                 else:
